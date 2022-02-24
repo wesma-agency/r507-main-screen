589de7f30287e1147c054a870e6a2f91520d8088
Igor Melnikov <igor2354@yandex.ru>
2022-02-24 10:35:29 +0300

    4

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 gsap.set(".anim-bg", { position: "absolute", width: "100%", height: "100%", top: 0, left: "0" });
 
-gsap.set(".scrollDist", { width: "100%", height: "120%" });
+gsap.set(".scrollDist", { width: "100%", height: "110%" });
 
 gsap.timeline({ scrollTrigger: { trigger: ".scrollDist", start: "top top", end: "bottom bottom", scrub: 1 } })
-	.fromTo(".anim-bg__mountain.--one", { y: 0 }, { y: -100 }, 0)
-	.fromTo(".anim-bg__mountain.--two", { y: 0 }, { y: -150 }, 0)
-	.fromTo(".anim-bg__mountain.--three", { y: 0 }, { y: -120 }, 0)
-	.fromTo(".anim-bg__coud.--one", { y: 0 }, { y: -50 }, 0)
+	.fromTo(".anim-bg__mountain.--one", { y: 0 }, { y: -30 }, 0)
+	.fromTo(".anim-bg__mountain.--two", { y: 0 }, { y: -250 }, 0)
+	.fromTo(".anim-bg__mountain.--three", { y: 0 }, { y: -200 }, 0)
+	.fromTo(".anim-bg__coud.--one", { y: 0 }, { y: -150 }, 0)
 	.fromTo(".anim-bg__coud.--two", { y: 0 }, { y: -100 }, 0)
 	.fromTo(".anim-bg__coud.--three", { y: 0 }, { y: -30 }, 0);
 
