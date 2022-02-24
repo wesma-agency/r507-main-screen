6d72d17db35d240e851dab9aaa7932b0b55b67b7
Igor Melnikov <igor2354@yandex.ru>
2022-02-24 10:38:14 +0300

    5

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ gsap.set(".anim-bg", { position: "absolute", width: "100%", height: "100%", top:
 gsap.set(".scrollDist", { width: "100%", height: "110%" });
 
 gsap.timeline({ scrollTrigger: { trigger: ".scrollDist", start: "top top", end: "bottom bottom", scrub: 1 } })
-	.fromTo(".anim-bg__mountain.--one", { y: 0 }, { y: -30 }, 0)
+	.fromTo(".anim-bg__mountain.--one", { y: 0 }, { y: -60 }, 0)
 	.fromTo(".anim-bg__mountain.--two", { y: 0 }, { y: -250 }, 0)
 	.fromTo(".anim-bg__mountain.--three", { y: 0 }, { y: -200 }, 0)
-	.fromTo(".anim-bg__coud.--one", { y: 0 }, { y: -150 }, 0)
+	.fromTo(".anim-bg__coud.--one", { y: 0 }, { y: -250 }, 0)
 	.fromTo(".anim-bg__coud.--two", { y: 0 }, { y: -100 }, 0)
 	.fromTo(".anim-bg__coud.--three", { y: 0 }, { y: -30 }, 0);
 
