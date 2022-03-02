885157bcd18710aa42917be8a749c84ec5ecd9d6
Igor Melnikov <igor2354@yandex.ru>
2022-03-02 18:13:08 +0300

    11

--- js/script.js
+++ js/script.js
@@ -2,15 +2,16 @@ gsap.set(".anim-bg", { position: "absolute", width: "100%", height: "100%", top:
 
 gsap.set(".scrollDist", { width: "100%", height: "110%" });
 
-gsap.timeline({ scrollTrigger: { trigger: ".scrollDist", start: "top top", end: "bottom bottom", scrub: 1 } })
+let tl = gsap
+	.timeline({
+		scrollTrigger: { trigger: ".scrollDist", start: "top top", end: "bottom bottom", scrub: 1 },
+	})
+	.fromTo(".sect-service__text", { duration: 0.3, color: "#fff" }, { duration: 0.3, color: "#000" }, 0)
+	.fromTo(".sect-service__link", { duration: 0.3, color: "#fff" }, { duration: 0.3, color: "#000" }, 0)
+	.fromTo(".sect-service__link svg path", { duration: 0.3, fill: "#fff" }, { duration: 0.3, fill: "#000" }, 0)
 	.fromTo(".anim-bg__mountain.--one", { y: 0 }, { y: -60 }, 0)
 	.fromTo(".anim-bg__mountain.--two", { y: 0 }, { y: -250 }, 0)
 	.fromTo(".anim-bg__mountain.--three", { y: 0 }, { y: -200 }, 0)
 	.fromTo(".anim-bg__coud.--one", { y: 0 }, { y: -250 }, 0)
 	.fromTo(".anim-bg__coud.--two", { y: 0 }, { y: -100 }, 0)
 	.fromTo(".anim-bg__coud.--three", { y: 0 }, { y: -30 }, 0);
-
-// .fromTo(".sky", { y: 0 }, { y: -200 }, 0)
-// .fromTo(".cloud1", { y: 100 }, { y: -800 }, 0)
-// .fromTo(".cloud2", { y: -150 }, { y: -500 }, 0)
-// .fromTo(".cloud3", { y: -50 }, { y: -650 }, 0)
